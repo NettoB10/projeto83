33d3fda5ebbfed0b582dec948cd3a8e0af6476a5
NettoB10 <nettobonatte@hotmail.com>
2026-04-30 00:41:51 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "projeto83-v1";
+
+const FILES = [
+  "index.html",
+  "manifest.json",
+  "icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
